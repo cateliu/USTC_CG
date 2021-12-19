ff9b17d0d46061deadeb4d467eefd201dd8e4f85
cateliu <971145957@qq.com>
2021-12-19 21:48:49 +0800

    参照sample代码后，修改的<list>多项式类

--- Homeworks/0_CppPratices/project/src/libraries/shared/PolynomialList.cpp
+++ Homeworks/0_CppPratices/project/src/libraries/shared/PolynomialList.cpp
@@ -23,8 +23,9 @@ PolynomialList::PolynomialList(const double* cof, const int* deg, int n) {
     // TODO
     for (int i = 0; i < n; i++)
     {
-        this->m_Polynomial.push_back(Term(*deg, *cof));
+        this->m_Polynomial.push_back(Term(deg[i], cof[i]));
     }
+    this->compress();
 }
 
 PolynomialList::PolynomialList(const vector<int>& deg, const vector<double>& cof) {
@@ -37,51 +38,77 @@ PolynomialList::PolynomialList(const vector<int>& deg, const vector<double>& cof
 
 double PolynomialList::coff(int i) const {
     // TODO
-    return 0.; // you should return a correct value
+    for (auto it : this->m_Polynomial)
+    {
+        if (it.deg == i)
+        {
+            return it.cof;
+        }
+        return 0.0;
+    }
+    // you should return a correct value
 }
 
 double& PolynomialList::coff(int i) {
     // TODO
-    static double ERROR; // you should delete this line
-    return ERROR; // you should return a correct value
+    //static double ERROR; // you should delete this line
+    double t = this->AddOneTerm(Term(i, 0)).cof;
+    this->compress();
+    return t; // you should return a correct value
 }
 
 void PolynomialList::compress() {
     // TODO
+    auto it = this->m_Polynomial.begin();
+    while (it != this->m_Polynomial.end())
+    {
+        if (fabs(it->cof) < 1.0e-10)
+        {
+            it = this->m_Polynomial.erase(it);
+        }
+        else
+        {
+            it++;
+        }
+    }
 }
 
 PolynomialList PolynomialList::operator+(const PolynomialList& right) const {
     // TODO
     PolynomialList Poly(*this);
-    for (auto it = right.m_Polynomial.begin(); it != right.m_Polynomial.end(); it++)
+
+    for (auto it : right.m_Polynomial)
     {
-        Term T1((*it).deg, (*it).cof);
+        Term T1((it).deg, (it).cof);
         Poly.AddOneTerm(T1);
     }
+    Poly.compress();
     return Poly; // you should return a correct value
 }
 
 PolynomialList PolynomialList::operator-(const PolynomialList& right) const {
     // TODO
     PolynomialList Poly(*this);
-    for (auto it = right.m_Polynomial.begin(); it != right.m_Polynomial.end(); it++)
+    for (auto it : right.m_Polynomial)
     {
-        Term T1((*it).deg, -(*it).cof);
+        Term T1((it).deg, -(it).cof);
         Poly.AddOneTerm(T1);
     }
+    Poly.compress();
     return Poly; // you should return a correct value
 }
 
 PolynomialList PolynomialList::operator*(const PolynomialList& right) const {
     // TODO
     PolynomialList Poly;
-    for (auto it = this->m_Polynomial.begin(); it != this->m_Polynomial.end(); it++)
+    for (auto it : this->m_Polynomial)
     {
-        for (auto itt = right.m_Polynomial.begin(); itt != right.m_Polynomial.end(); itt++)
+        for (auto itt : right.m_Polynomial)
         {
-            Poly.AddOneTerm(Term((*it).deg + (*itt).deg, (*it).cof * (*itt).cof));
+            Poly.AddOneTerm(Term(it.deg + itt.deg, it.cof * itt.cof));
         }
     }
+    Poly.compress();
     return Poly; // you should return a correct value
 }
 
@@ -93,11 +120,53 @@ PolynomialList& PolynomialList::operator=(const PolynomialList& right) {
 
 void PolynomialList::Print() const {
     // TODO
-    for (auto it = this->m_Polynomial.begin(); it != this->m_Polynomial.end(); it++)
+    if (this->m_Polynomial.empty())
     {
-        cout << "第" << (*it).deg << "项数的系数为：" << (*it).cof << endl;
+        cout << 0 << endl;
+        return;
+    }
+    auto it = this->m_Polynomial.begin();
+    if (it->cof >= 0)
+    {
+        cout << it->cof;
+    }
+    else
+    {
+        cout << -it->cof;
+    }
+
+    cout << "x^";
+
+    if (it->deg >= 0)
+    {
+        cout << it->deg;
+    }
+    else
+    {
+        cout << -it->deg;
+    }
+    
+    it++;
+    for (; it != this->m_Polynomial.end(); it++)
+    {
+        
+        if (it->cof >= 0)
+        {
+            cout << " + " << it->cof;
+        }
+        else
+        {
+            cout << " - " << -it->cof;
+        }
+
+        cout << "x^" << it->deg;
+
     }
     cout << endl;
+    //for (auto it = this->m_Polynomial.begin(); it != this->m_Polynomial.end(); it++)
+    //{
+    //    cout << "第" << (*it).deg << "项数的系数为：" << (*it).cof << endl;
+    //}
 }
 
 bool PolynomialList::ReadFromFile(const string& file) {
@@ -140,15 +209,14 @@ bool PolynomialList::ReadFromFile(const string& file) {
 PolynomialList::Term& PolynomialList::AddOneTerm(const Term& term) {
     // TODO
     // cout << "deg:" << term.deg << " cof:" << term.cof << endl;
-    static Term ERROR; // you should delete this line
-    if (this->m_Polynomial.empty())
+
+    if (this->m_Polynomial.empty() || term.deg < this->m_Polynomial.back().deg)
     {
+        static Term T1(term.deg, term.cof); // you should delete this line
         this->m_Polynomial.push_back(term);
+        return T1;
     }
-    else if (term.deg < this->m_Polynomial.back().deg)
-    {
-        this->m_Polynomial.push_back(term);
-    }
+    
     else 
     {
         for (auto it = this->m_Polynomial.begin(); it != this->m_Polynomial.end(); it++)
@@ -156,16 +224,17 @@ PolynomialList::Term& PolynomialList::AddOneTerm(const Term& term) {
 
             if (term.deg == (*it).deg)
             {
+                static Term T1(term.deg, term.cof + (*it).cof);
                 (*it).cof = (*it).cof + term.cof;
-                break;
+                return T1;
             }
             else if (term.deg > (*it).deg)
             {
+                static Term T1(term.deg, term.cof);
                 this->m_Polynomial.insert(it, term);
-                break;
+                return T1;
             }
         }
     }
-
-    return ERROR; // you should return a correct value
+    //return ERROR; // you should return a correct value
 }
